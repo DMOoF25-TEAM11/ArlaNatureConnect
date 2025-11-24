7cc06f82ba24ede85489f899420082db1387b25f
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-11-24 17:18:04 +0800

    Add viewmodels

---
+++ src/ArlaNatureConnect.WinUI/ArlaNatureConnect.WinUI/ViewModels/Controls/SideMenu/AdministratorPageSideMenuUCViewModel.cs
@@ -0,0 +1,7 @@
+using ArlaNatureConnect.WinUI.ViewModels.Abstracts;
+
+namespace ArlaNatureConnect.WinUI.ViewModels.Controls.SideMenu;
+
+public class AdministratorPageSideMenuUCViewModel : ViewModelBase
+{
+}
--- src/ArlaNatureConnect.WinUI/ArlaNatureConnect.WinUI/Views/Pages/AdministratorPage.xaml.cs
+++ src/ArlaNatureConnect.WinUI/ArlaNatureConnect.WinUI/Views/Pages/AdministratorPage.xaml.cs
@@ -1,17 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
+using ArlaNatureConnect.Core.Abstract;
+using ArlaNatureConnect.WinUI.Services;
+using ArlaNatureConnect.WinUI.Views.Controls.Abstracts;
+
+using Microsoft.Extensions.DependencyInjection;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -21,10 +12,16 @@ namespace ArlaNatureConnect.WinUI.Views.Pages;
 /// <summary>
 /// An empty page that can be used on its own or navigated to within a Frame.
 /// </summary>
-public sealed partial class AdministratorPage : Page
+public sealed partial class AdministratorPage : NavPage
 {
     public AdministratorPage()
     {
         InitializeComponent();
+
+        // Get dependencies from App's service provider
+        NavigationHandler navigationHandler = App.HostInstance.Services.GetRequiredService<NavigationHandler>();
+        IPersonRepository personRepository = App.HostInstance.Services.GetRequiredService<IPersonRepository>();
+        IRoleRepository roleRepository = App.HostInstance.Services.GetRequiredService<IRoleRepository>();
+
     }
 }
